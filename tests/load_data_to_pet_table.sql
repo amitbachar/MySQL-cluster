5736b0eef1706aa94cf14e57bfcf2a12ca286e3a
Amit Bachar <amit.bachar@gmail.com>
2018-07-25 15:17:48 +0300

    Update load_data_to_pet_table.sql

--- tests/load_data_to_pet_table.sql
+++ tests/load_data_to_pet_table.sql
@@ -1,2 +1,3 @@
-#LOAD DATA LOCAL INFILE '/tmp/pet.txt' into table pet LINES TERMINATED BY '\r\n';
+show tables;
 LOAD DATA LOCAL INFILE '/tmp/pet.txt' into TABLE pet fields terminated by ',' lines terminated by '\n';
+select * from pet;
